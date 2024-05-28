da0abe578850b327b00cae92da8be90b215e5b66
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-28 11:39:32 -0400

    Create list after title or author search on submit
    The results need to be narrowed down in the next story, the search is very broad at the moment

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   let form = document.getElementById("book-form")
+  let titleList = document.getElementById("book-title-list")
 
   form.addEventListener("submit", (e) => {
     e.preventDefault()
+    titleList.innerHTML = ""
     queryValue = document.getElementById("search").value
 
     fetch("https://openlibrary.org/search.json?q=" + queryValue, {
     })
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      createBookTitles(data)
     })
   })
+
+  const createBookTitles = (book) => {
+    book.docs.forEach((doc, index) => {
+      const title = document.createElement("li")
+      title.textContent = `Index ${index}: ${doc.title}`
+      titleList.appendChild(title)
+    })
+  }
 
 })
