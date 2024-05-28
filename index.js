10ae2d1b12f3e7c761a5f2250d550a82d970dd77
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-28 10:59:05 -0400

    Create search bar and hit api, console log results

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+document.addEventListener("DOMContentLoaded", () => {
+
+  let form = document.getElementById("book-form")
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    queryValue = document.getElementById("search").value
+
+    fetch("https://openlibrary.org/search.json?q=" + queryValue, {
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+  })
+
+})
