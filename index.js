dd38ea06f357bd8b1ba7d85dc02c4cc7c2d385d8
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-28 14:28:02 -0400

    Remove ISBN in exchange for cover_i, more unique id to get covers

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ document.addEventListener("DOMContentLoaded", () => {
     titleList.innerHTML = ""
     queryValue = document.getElementById("search").value.trim();
 
-    fetch(`https://openlibrary.org/search.json?author=${queryValue}&fields=key,title,author_name,editions,isbn&limit=20`, {
+    fetch(`https://openlibrary.org/search.json?author=${queryValue}&fields=key,title,author_name,editions,cover_i&limit=20`, {
     })
     .then(response => response.json())
     .then(data => {
       createBookTitles(data)
-      console.log(data)
     })
   })
 
   const createBookTitles = (book) => {
     book.docs.forEach((doc, index) => {
       const title = document.createElement("li")
-      title.textContent = `Book Title: ${doc.title} | Author: ${doc.author_name} | ISBN: ${doc.isbn[1]}`
+      const bookCover = document.createElement("img")
+
+      bookCover.src = `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg`
+      title.textContent = `Book Title: ${doc.title} | Author: ${doc.author_name}`
+
+      titleList.appendChild(bookCover)
       titleList.appendChild(title)
     })
   }
