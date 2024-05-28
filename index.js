83612484efcb05df46ffd7163d03e20d288de319
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-28 15:22:41 -0400

    Create css and html for book grid, limit to 5 books per row

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   let form = document.getElementById("book-form")
-  let titleList = document.getElementById("book-title-list")
+  let coverImage = document.querySelector(".book-grid")
 
   form.addEventListener("submit", (e) => {
     e.preventDefault()
-    titleList.innerHTML = ""
+    coverImage.innerHTML = ""
     queryValue = document.getElementById("search").value.trim();
 
     fetch(`https://openlibrary.org/search.json?author=${queryValue}&fields=key,title,author_name,editions,cover_i&limit=20`, {
@@ -18,14 +18,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const createBookTitles = (book) => {
     book.docs.forEach((doc, index) => {
-      const title = document.createElement("li")
       const bookCover = document.createElement("img")
-
       bookCover.src = `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg`
-      title.textContent = `Book Title: ${doc.title} | Author: ${doc.author_name}`
-
-      titleList.appendChild(bookCover)
-      titleList.appendChild(title)
+      coverImage.appendChild(bookCover)
     })
   }
 
