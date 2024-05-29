6ddeba5e183633c3a4f0215b27da3af1e8e66183
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-29 10:52:39 -0400

    Clean up css and mouseover event

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  e.preventDefault()
   let form = document.getElementById("book-form")
   let coverImage = document.querySelector(".book-grid")
   let bookDetails = document.querySelector('.book-details-container')
@@ -9,7 +8,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     coverImage.innerHTML = ""
     queryValue = document.getElementById("search").value.trim();
 
-    fetch(`https://openlibrary.org/search.json?author=${queryValue}&fields=key,title,author_name,editions,cover_i,ratings_average,subject&limit=20`, {
+    fetch(`https://openlibrary.org/search.json?author=${queryValue}&fields=key,title,author_name,editions,cover_i,ratings_average,subject,first_publish_year&limit=20`, {
     })
     .then(response => response.json())
     .then(data => {
@@ -34,7 +33,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
         detailsCover.src = `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg`
         title.textContent = `Title: ${doc.title}`
         avgRating.textContent = `Average Rating: ${doc.ratings_average}`
-        genre.textContent = `Genre: ${(doc.subject.slice(0,3))}`
+        genre.textContent = `Genre: ${(doc.subject.slice(0,3))} `
 
         bookDetails.appendChild(detailsCover)
         bookDetails.appendChild(title)
@@ -42,21 +41,23 @@ document.addEventListener("DOMContentLoaded", (e) => {
         bookDetails.appendChild(genre)
 
         detailsCover.addEventListener("mouseover", (e) => {
-          // const overlayText = document.createElement("div");
-          // overlayText.textContent = `${doc.author_name}`;
+          const overlayText = document.createElement("div");
+          overlayText.textContent = `First published in ${doc.first_publish_year}.`;
           detailsCover.classList.add('overlay-hover')
-          // overlayText.classList.add("overlay-text");
-          // e.currentTarget.parentElement.appendChild(overlayText);
+          overlayText.classList.add("overlay-text");
+          e.currentTarget.parentElement.appendChild(overlayText);
 
         })
         detailsCover.addEventListener("mouseout", (e) => {
           detailsCover.classList.remove('overlay-hover');
-          // const overlayText = e.currentTarget.parentElement.querySelector(".overlay-text");
-          // if (overlayText) {
-          //   overlayText.remove();
-          // }
+          const overlayText = e.currentTarget.parentElement.querySelector(".overlay-text");
+          if (overlayText) {
+            overlayText.remove();
+          }
         })
       })
     })
   }
+
+
 })
